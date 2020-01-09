5ca1582dac46c0fcf11e751bad457a029e5eacd6
ecxgitdocker <38083449+mustafaecentrix@users.noreply.github.com>
2020-01-09 15:59:21 +0700

    Update size_per_table.sh

--- size_per_table.sh
+++ size_per_table.sh
@@ -1,5 +1,8 @@
-SELECT table_name AS 'Table',FORMAT(table_rows,0) AS 'Quant of Rows',FORMAT(
+QUERY="SELECT table_name AS 'Table',FORMAT(table_rows,0) AS 'Quant of Rows',FORMAT(
 round(((data_length + index_length) / 1024 / 1024), 0),0) 'Size in MB' FROM
  information_schema.TABLES WHERE table_schema = '${DB_NAME}' 
 AND TABLE_NAME IN ('table1','table2')
-ORDER BY (data_length + index_length) DESC;
+ORDER BY (data_length + index_length) DESC;"
+
+mysql -Bse "${QUERY}"
+
